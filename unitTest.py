6bfabe171a92fc467f8d083a6df6015ddba805ee
Josh Kleven <joshkleven09@gmail.com>
2016-10-11 18:21:35 -0500

    Added second failing test.

--- unitTest.py
+++ unitTest.py
@@ -1,7 +1,13 @@
 import unittest
 import HelloWorld as HW
 
+def test_HelloWorldWrong():
+    result = HW.printHelloWorld()
+    assert result == "Hello World"
+    
 def test_HelloWorld():
     result = HW.printHelloWorld()
     assert result == "Hello World!"
+
+
 
